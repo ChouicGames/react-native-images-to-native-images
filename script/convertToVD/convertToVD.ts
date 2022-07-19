bc8005c1b8fb9b3480dad70b60f2addc5c325c34
Wexop <78621943+Wexop@users.noreply.github.com>
2022-07-19 15:30:17 +0200

    Fix uri name of VD

--- script/convertToVD/convertToVD.ts
+++ script/convertToVD/convertToVD.ts
@@ -1,9 +1,11 @@
 import { folderExist } from "../baseCode/common"
 import * as readline from "readline"
-
+var path = require("path")
+var shell = require("shelljs")
 const { vdConvert } = require("vd-tool")
 
 export const convertSvgToVD = async (pathFile: string, outputFilePath: string, fileUri: string) => {
+  const fileXmlName = path.parse(pathFile).base
   const fileName = fileUri + ".xml"
   const finalOutput = outputFilePath + "drawable/"
 
@@ -12,7 +14,9 @@ export const convertSvgToVD = async (pathFile: string, outputFilePath: string, f
   process.stdout.write("⚙️  CONVERT XML START IN " + finalOutput + fileName)
 
   await vdConvert(pathFile, { outDir: finalOutput })
-
+  shell.exec(
+    "mv " + finalOutput + fileXmlName.replace(".svg", ".xml") + " " + finalOutput + fileName
+  )
   readline.cursorTo(process.stdout, 0)
   process.stdout.write("✅ CONVERT XML DONE \n")
 }
