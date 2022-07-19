c52a9cccde737f4e82e63063b0d577e65625b576
Wexop <78621943+Wexop@users.noreply.github.com>
2022-07-19 17:07:17 +0200

    Update example dependencies

--- Example/images/Images.ts
+++ Example/images/Images.ts
@@ -1,9 +1,9 @@
 import { AndroidFileType, IosFileType } from "react-native-images-to-native-images/src/types"
-import { Files, FilesInfos } from "./types"
+import { Files, FilesInfos } from "./ImagesTypes"
 
 export const appName: string = "Example" // <------ change to your app name (in YourProject/ios/YourAppName)
 
-export const files: FilesInfos = {
+export const Images: FilesInfos = {
   //YOUR IMAGES HERE !
 
   // Follow this example :
@@ -45,3 +45,5 @@ export const files: FilesInfos = {
     ios: { type: IosFileType.Pdf },
   },
 }
+
+export default Images
