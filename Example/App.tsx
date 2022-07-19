eb005b2d269db22b3667adfbf3acddf01170d20b
Wexop <78621943+Wexop@users.noreply.github.com>
2022-07-19 16:30:14 +0200

    Fix generation from default Images.ts

--- Example/App.tsx
+++ Example/App.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { ScrollView, View } from "react-native"
 
 import { NativeImage } from "react-native-images-to-native-images"
-import { Files } from "./images/types"
+import Images from "./images/Images"
 
 const App = () => {
   const imagesSize = {
@@ -15,13 +15,13 @@ const App = () => {
   return (
     <ScrollView>
       <View>
-        <NativeImage file={Files.IMG0} style={imagesSize} />
+        <NativeImage file={Images.IMG0} style={imagesSize} />
         <View style={{ borderColor: "black", borderWidth: 4 }}>
-          <NativeImage file={Files.IMG1} style={imagesSize} />
+          <NativeImage file={Images.IMG1} style={imagesSize} />
         </View>
-        <NativeImage file={Files.IMG2} style={imagesSize} />
-        <NativeImage file={Files.IMG3} style={imagesSize} />
-        <NativeImage file={Files.IMG4} style={imagesSize} />
+        <NativeImage file={Images.IMG2} style={imagesSize} />
+        <NativeImage file={Images.IMG3} style={imagesSize} />
+        <NativeImage file={Images.IMG4} style={imagesSize} />
       </View>
     </ScrollView>
   )
--- script/index.ts
+++ script/index.ts
@@ -8,7 +8,7 @@ export const index = () => {
       "tsc ./images/Images.ts --outDir ./node_modules/react-native-images-to-native-images/lib/commonImage/"
     )
     var constantsFile = require("./commonImage/Images")
-    imagesGenerator(constantsFile["files"], constantsFile["appName"])
+    imagesGenerator(constantsFile["default"], constantsFile["appName"])
   } else {
     console.log("CANNOT START")
     console.log("run generate-images-init to create 'images/' folder with 'Images' + 'Types' files")
