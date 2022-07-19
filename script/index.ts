f1ca312b7d10cc8621b06ab8e254884f81144a9f
Wexop <78621943+Wexop@users.noreply.github.com>
2022-07-19 15:59:47 +0200

    Update index for Images.ts

--- script/index.ts
+++ script/index.ts
@@ -5,15 +5,13 @@ var shell = require("shelljs")
 export const index = () => {
   if (fs.existsSync("./images/")) {
     shell.exec(
-      "tsc ./images/constants.ts --outDir ./node_modules/react-native-images-to-native-images/lib/commonImage/"
+      "tsc ./images/Images.ts --outDir ./node_modules/react-native-images-to-native-images/lib/commonImage/"
     )
     var constantsFile = require("./commonImage/constants")
     imagesGenerator(constantsFile["files"], constantsFile["appName"])
   } else {
     console.log("CANNOT START")
-    console.log(
-      "run generate-images-init to create 'images/' folder with 'constants' + 'types' files"
-    )
+    console.log("run generate-images-init to create 'images/' folder with 'Images' + 'Types' files")
   }
 }
 
