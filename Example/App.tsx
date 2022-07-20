02792011dd910db1d0c9583a49a24ea3ac1bade7
Wexop <78621943+Wexop@users.noreply.github.com>
2022-07-20 09:40:05 +0200

    Update example

--- Example/App.tsx
+++ Example/App.tsx
@@ -3,6 +3,7 @@ import { ScrollView, View } from "react-native"
 
 import { NativeImage } from "react-native-images-to-native-images"
 import Images from "./images/Images"
+import { Files } from "./images/ImagesTypes"
 
 const App = () => {
   const imagesSize = {
@@ -15,13 +16,13 @@ const App = () => {
   return (
     <ScrollView>
       <View>
-        <NativeImage file={Images.IMG0} style={imagesSize} />
+        <NativeImage file={Images[Files.IMG0]} style={imagesSize} />
         <View style={{ borderColor: "black", borderWidth: 4 }}>
-          <NativeImage file={Images.IMG1} style={imagesSize} />
+          <NativeImage file={Images[Files.IMG1]} style={imagesSize} />
         </View>
-        <NativeImage file={Images.IMG2} style={imagesSize} />
-        <NativeImage file={Images.IMG3} style={imagesSize} />
-        <NativeImage file={Images.IMG4} style={imagesSize} />
+        <NativeImage file={Images[Files.IMG2]} style={imagesSize} />
+        <NativeImage file={Images[Files.IMG3]} style={imagesSize} />
+        <NativeImage file={Images[Files.IMG4]} style={imagesSize} />
       </View>
     </ScrollView>
   )
